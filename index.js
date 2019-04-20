d8de7c75f22b71a26e940dc9b13373ac375968d8
Matthew Listor <mattlistor@gmail.com>
2019-04-20 18:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ function theBeatlesPlay(players, instruments ) {
   for (var counter = 0; counter < players.length; counter++){
     arrayOfStrings.push(players[counter] + " plays " + instruments[counter])
   }
-  return array;
+  return arrayOfStrings
+arrayOfStrings;
 }
